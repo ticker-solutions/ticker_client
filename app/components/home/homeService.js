a4fc43ab00a7385876a46052be8a5fff7bbd8ac2
Garry Newball <gnewball@ticker.solutions>
2015-04-11 17:44:59 -0500

    url service base changed

--- app/components/home/homeService.js
+++ app/components/home/homeService.js
@@ -3,7 +3,7 @@ angular.module('tickerApp').factory('logInService', logInService);
 logInService.$inject = ['$resource'];
 
 function logInService($resource) {
-    return $resource('http://localhost:8080/TickerCore/rest/personEndpoint', {}, {
+    return $resource('http://ticker.solutions:8484/TickerCore-0.0.1-SNAPSHOT/rest/personEndpoint', {}, {
         create: {
             method: 'POST'
         }
